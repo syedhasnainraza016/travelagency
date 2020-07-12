4e4f2c800a23cc17fe85c708e3eba4d44645a6c9
hasnain <iamsyedhasnainraza@gmail.com>
2020-07-12 14:10:26 +0500

    commit

--- config/keys.js
+++ config/keys.js
@@ -1,9 +1,1 @@
-<<<<<<< HEAD
-dbPassword = 'mongodb://localhost:27017/Shopper'
-=======
-dbPassword = 'mongodb+srv://uos:uos@uostrvaelagency-dvkbv.mongodb.net/test?retryWrites=true&w=majority'
->>>>>>> 1c018a2b3648d894018b81306578b374efb2523d
 
-module.exports = {
-    mongoURI: dbPassword
-};
